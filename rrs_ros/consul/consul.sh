ad63d41719d0dd2f5f4225fad8be0ee31aef9335
Premankur Banerjee <ge46paq@mytum.de>
2020-12-14 13:06:51 +0100

    Delete duplicated packages after src folder.

--- rrs_ros/consul/consul.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./consul agent -dev -config-dir=data -client="0.0.0.0"
--- rrs_ros/movo/movo_robot/movo_upstart/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup(**generate_distutils_setup(
-    packages=['movo_upstart'],
-    package_dir={'': 'src'}
-))
